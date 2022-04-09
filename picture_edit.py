aadd54093021914c66e8f61bcd1b39e2fdfbeeda
khadada <lkhadada@gmail.com>
2022-04-09 13:56:52 +0100

    I imported necessary classes and modules

---
+++ picture_edit.py
@@ -0,0 +1,6 @@
+# Import necessary module and classes:
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow,QWidget,QLabel,QAction,QFileDialog,QDesktopWidget,QMessageBox,QSizePolicy,QToolBar,QStatusBar,QDockWidget,QVBoxLayout,QPushButton)
+from PyQt5.QtGui import(QIcon, QPixmap,QTransform,QPainter)
+from PyQt5.QtCore import (Qt, QSize, QRect)
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
